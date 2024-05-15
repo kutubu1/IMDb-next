25653f1464c05f60772019506d004af6e13bfcb7
Abdul Wadudu Shaibu <zutfoom@gmail.com>
2024-05-15 14:32:31 +0000

    handling error

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,33 @@
+import Results from "./components/Results";
 
+const API_KEY = process.env.API_KEY;
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  let results = [];
+  try {
+    const genre = searchParams?.genre || 'fetchTrending';
+    const endpoint =
+      genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week';
+    
+    const res = await fetch(
+      `https://api.themoviedb.org/3/${endpoint}?api_key=${API_KEY}&language=en-US&page=1`
+    );
+
+    if (!res.ok) {
+      throw new Error('Something went wrong');
+    }
+
+    const data = await res.json();
+    results = data.results;
+    console.log(results);
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+  }
+
   return (
-   <>
-    <h1>Hello World</h1>
-   </>
+    <div>
+      <h1>Home</h1>
+      <Results results={results} />
+    </div>
   );
 }
